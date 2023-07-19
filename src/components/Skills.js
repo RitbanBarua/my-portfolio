992b4e770cff7d41a47ccb590a131b0a1ef17684
RitbanBarua <ritbanbarua2@gmail.com>
2023-07-19 20:05:27 +0530

    Added hide animation on scroll

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,19 +11,19 @@ export default function Skills() {
         <div className='container' id='skills-container'>
           <h2 className='section-title reveal'>Skills</h2>
           <div className="container" id='skills-img-container'>
-            <div className='img-wrapper reveal' id="html-img-wrapper">
+            <div className='skills-img-wrapper reveal' id="html-img-wrapper">
               <img src={htmlLogo} alt="HTML5" />
               <div className="hover-container">HTML</div>
             </div>
-            <div className='img-wrapper reveal' id="css-img-wrapper">
+            <div className='skills-img-wrapper reveal' id="css-img-wrapper">
               <img src={cssLogo} alt="CSS" />
               <div className="hover-container">CSS</div>
             </div>
-            <div className='img-wrapper reveal' id="js-img-wrapper">
+            <div className='skills-img-wrapper reveal' id="js-img-wrapper">
               <img src={jsLogo} alt="JAVASCRIPT" />
               <div className="hover-container">Javascript</div>
             </div>
-            <div className='img-wrapper reveal' id="react-img-wrapper">
+            <div className='skills-img-wrapper reveal' id="react-img-wrapper">
               <img src={reactLogo} alt="REACT" />
               <div className="hover-container">React</div>
             </div>
